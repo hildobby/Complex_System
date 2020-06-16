d872368619e70089b7aef91f9a5ab6dc7122726d
Louis Weyland <weylandlouis@gmail.com>
2020-06-16 15:27:54 +0200

    - Added a function to make the plots uniform

---
+++ src/plotting_functions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Created on Tuesday June 16 2020
+This code was implemented by
+Louis Weyland, Hildebert Mouilé, Philippe Nicolau & Binjie Zhou.
+"""
+
+
+def plot_setting():
+    """
+    Change the default matplotlib parameters to make sure the graphs are unifrom
+    """
+    params = {'legend.fontsize': 'x-large',
+              'figure.figsize': (8, 6),
+              'axes.labelsize': 'x-large',
+              'axes.titlesize': 'x-large',
+              'xtick.labelsize': 'x-large',
+              'ytick.labelsize': 'x-large'}
+    plt.rcParams.update(params)
