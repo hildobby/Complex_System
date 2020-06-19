4c0e77a16e1164c3683fca0e5b223e94dd39f667
Louis Weyland <weylandlouis@gmail.com>
2020-06-19 16:52:47 +0200

    - fixed some typos

--- src/lattice.py
+++ src/lattice.py
@@ -28,7 +28,7 @@ class Lattice():
                  torus_mode=True,
                  neighbourhood='vonNeumann',
                  distance='euclidean',
-                 free_precent=0.1):
+                 free_percent=0.1):
         """
         Creates the Graph
         :param size: if type is a 2d graph size needs to be tuple, if type= grid_graph size is a list []
@@ -43,7 +43,8 @@ class Lattice():
         self.random_dist,*self.random_dist_specification = rand_dist
         self.neighbourhood = neighbourhood
         self.distance_btw_neighbours = distance   # either "networkx" or "euclidean"
-        self.free_precent = free_precent
+        self.free_percent = free_percent
+
 
         # initialising counters and single variable
         self.time_step = 0
@@ -63,38 +64,31 @@ class Lattice():
         self.check_error()
 
 
-    def fitness_init(self):
+    def fitness_init(self,node):
         """
         Initialize the fitness values to the graph using 3 different random distribution
         uniform, exponential and gaussian
         """
-        #if self.random_dist == 'uniform':
-        for node in self.lattice.nodes:
-                self.lattice.nodes[node]['fitness'] = random()
-        #elif self.random_dist == 'exponential':
-        #    for node in self.lattice.nodes:
-        #        self.lattice.nodes[node]['fitness'] = expovariate(self.random_dist_specification[0])
-        #elif self.random_dist == 'gauss':
-        #    for node in self.lattice.nodes:
-        #        self.lattice.nodes[node]['fitness'] = gauss(self.random_dist_specification[0],
-        #                                                          self.random_dist_specification[1])
+        self.lattice.nodes[node]['fitness'] = random()
 
-    def age_init(self):
+
+    def age_init(self,node):
         """
         Assigns an age to the nodes which is defined by the last time it underwent mutation
         """
-        for node in self.lattice.nodes:
-            self.lattice.nodes[node]['age'] = 0
-            
-    
-    
+        self.lattice.nodes[node]['age'] = 0
+
+
     def free_init(self):
         """
-        Assigns an if the node is free or not
+        Assigns if the node is free or not and add the fitness and age to it
+        It is calling function age_init and fitness_init
         """
         for node in self.lattice.nodes:
-            if random() > self.free_precent:
+            if random() > self.free_percent:
                 self.lattice.nodes[node]['is_free'] = False
+                self.fitness_init(node)
+                self.age_init(node)
             else:
                 self.lattice.nodes[node]['is_free'] = True
 
@@ -104,12 +98,11 @@ class Lattice():
         Update the age at each time step appart from the positions in the list pos
         :param pos: Pos is a list of cells that have been update so the respective nodes are set to zero
         """
-        for node in self.lattice.nodes:
+        for node in nx.get_node_attributes(lattice.lattice,'age').keys():
             if node in self.latest_mutation_pos_list:
                 self.lattice.nodes[node]['age'] = 0
             elif node not in self.latest_mutation_pos_list:
                 self.lattice.nodes[node]['age'] += 1
-
 
 
     def get_min(self):
@@ -129,7 +122,7 @@ class Lattice():
         elif self.min_value > max(self.threshold_list):
             self.threshold_list.append(self.min_value)
 
-    def get_avergae(self):
+    def get_average(self):
         """
         Get the average fitness/age value
         """
@@ -152,12 +145,12 @@ class Lattice():
             if self.min_pos[0]==0:
                 self.neighbours.append((self.size[0]-1,self.min_pos[1]))
             elif self.min_pos[0]==self.size[0]-1:
-                print((0,self.min_pos[1]))
+                #print((0,self.min_pos[1]))
                 self.neighbours.append((0,self.min_pos[1]))
-            print(self.neighbours)
+           # print(self.neighbours)
             
         elif self.neighbordhood ==  'Moore':
-            print(self.min_pos)
+            #print(self.min_pos)
             self.neighbours = list()
             # Calculate the neighbours for this object
             for x1 in range(-1,2):
@@ -249,8 +242,6 @@ class Lattice():
         For example avalanche time can work before mutation
         """
         # initialize the nodes with fitness and their age
-        self.fitness_init()
-        self.age_init()
         self.free_init()
 
         for i in range(iteration):
@@ -263,7 +254,7 @@ class Lattice():
             self.get_min()
 
             # gets the average fitness/age of all the nodes at each time_step
-            self.get_avergae()
+            self.get_average()
 
             # get the neighbours
             self.get_neighbours()
@@ -288,15 +279,15 @@ class Lattice():
         if label == 'fitness':
             values = set(nx.get_node_attributes(self.lattice, 'fitness').values())
             mapping = dict(zip(sorted(values), count()))
-            nodes = self.lattice.nodes()
+            nodes_fitness = nx.get_node_attributes(lattice.lattice,'fitness').keys()
 
             # Get the color code and normalise it
-            colors = [mapping[self.lattice.nodes[n]['fitness']] for n in nodes]
+            colors = [mapping[self.lattice.nodes[n]['fitness']] for n in nodes_fitness]
             colors = [color/(self.size[0]*self.size[1]) for color in colors]
 
             pos = dict( (n, n) for n in self.lattice.nodes() )
-            nc = nx.draw_networkx_nodes(self.lattice, pos, nodelist=nodes, node_color=colors,
-                                        with_labels=False, node_size=100, cmap=plt.cm.jet)
+            nc = nx.draw_networkx_nodes(self.lattice, pos, nodelist=nodes_fitness, node_color=colors,
+                                        with_labels=False, node_size=200,node_shape = 'h', cmap=plt.cm.jet)
             #plt.figure()
             #plt.colorbar(nc)
             #plt.axis('off')
@@ -304,14 +295,14 @@ class Lattice():
         elif label == 'age':
             values = set(nx.get_node_attributes(self.lattice, 'age').values())
             mapping = dict(zip(sorted(values), count()))
-            nodes = self.lattice.nodes()
+            nodes_ages = nx.get_node_attributes(lattice.lattice,'age').keys()
 
             # Get the color code and normalise it
-            colors = [mapping[self.lattice.nodes[n]['age']] for n in nodes]
+            colors = [mapping[self.lattice.nodes[n]['age']] for n in nodes_ages]
 
             pos = dict((n, n) for n in self.lattice.nodes())
-            nc = nx.draw_networkx_nodes(self.lattice, pos, nodelist=nodes, node_color=colors,
-                                        with_labels=False, node_size=100, cmap=plt.cm.cubehelix)
+            nc = nx.draw_networkx_nodes(self.lattice, pos, nodelist=nodes_ages, node_color=colors,
+                                        with_labels=False, node_size=200,node_shape = 'h', cmap=plt.cm.gnuplot)
             #plt.figure()
             #plt.colorbar(nc)
             #plt.axis('off')
@@ -334,7 +325,7 @@ if __name__ == "__main__":
     iterations = 2000
     t0 = time.time()
     # if rand_dist take 1 arg, rand_dist=('uniform',) !! Comma needed here
-    lattice = Lattice(size=(20,20),torus_mode=True,rand_dist=('uniform',))
+    lattice = Lattice(size=(20,20),torus_mode=True,rand_dist=('uniform',),free_percent=0.1)
     print(nx.info(lattice.lattice, n=None))
     lattice.run(iteration=iterations)
     t1 = time.time()
